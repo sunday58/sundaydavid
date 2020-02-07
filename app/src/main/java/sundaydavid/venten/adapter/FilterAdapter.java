53687d445c968bae065fd62f5e47e37a1fb3d561
sundaydavid <sundaydavid989@gmail.com>
2020-02-07 14:02:22 +0100

    Adjusted visibility for empty parameters

--- app/src/main/java/sundaydavid/venten/adapter/FilterAdapter.java
+++ app/src/main/java/sundaydavid/venten/adapter/FilterAdapter.java
@@ -39,42 +39,174 @@ public class FilterAdapter extends RecyclerView.Adapter<FilterAdapter.FilterView
     @Override
     public void onBindViewHolder(@NonNull FilterViewHolder holder, final int position) {
 
-        holder.gender.setText(ventenFilters.get(position).getGender());
-        holder.startYear.setText(String.format(String.valueOf(ventenFilters.get(position).getStartYear())));
-        holder.endYear.setText(String.format(String.valueOf(ventenFilters.get(position).getEndYear())));
+        if (ventenFilters.get(position).getGender().isEmpty() && ventenFilters.get(position)
+                .getCountries().isEmpty() && ventenFilters.get(position).getColors().isEmpty()){
+
+            holder.gender.setVisibility(View.GONE);
+            holder.genderData.setVisibility(View.GONE);
+            holder.countries.setVisibility(View.GONE);
+            holder.countriesData.setVisibility(View.GONE);
+            holder.colors.setVisibility(View.GONE);
+            holder.colorsData.setVisibility(View.GONE);
+
+            holder.startYear.setText(String.format(String.valueOf(ventenFilters.get(position).getStartYear())));
+            holder.endYear.setText(String.format(String.valueOf(ventenFilters.get(position).getEndYear())));
+
+        }else if (ventenFilters.get(position).getGender().isEmpty() &&
+                ventenFilters.get(position).getColors().isEmpty()){
+
+            holder.gender.setVisibility(View.GONE);
+            holder.genderData.setVisibility(View.GONE);
+            holder.colors.setVisibility(View.GONE);
+            holder.colorsData.setVisibility(View.GONE);
+
+            holder.startYear.setText(String.format(String.valueOf(ventenFilters.get(position).getStartYear())));
+            holder.endYear.setText(String.format(String.valueOf(ventenFilters.get(position).getEndYear())));
 
 
-        ArrayList<String>  arrayListCountry = new ArrayList<>();
-        arrayListCountry.add(ventenFilters.get(position).getCountries().toString());
-        StringBuilder builderCountry = new StringBuilder();
+            ArrayList<String> arrayListCountry = new ArrayList<>();
+            arrayListCountry.add(ventenFilters.get(position).getCountries().toString());
+            StringBuilder builderCountry = new StringBuilder();
 
-        for (String valueCountry : arrayListCountry){
-            builderCountry.append(valueCountry);
+            for (String valueCountry : arrayListCountry) {
+                builderCountry.append(valueCountry);
+
+            }
+            String formattedStringCountry = builderCountry.toString()
+                    .replace("[", "")
+                    .replace("]", "")
+                    .replace(",", "")
+                    .trim();
+            holder.countries.setText(formattedStringCountry);
+
+        } else if (ventenFilters.get(position).getGender().isEmpty()){
+            holder.gender.setVisibility(View.GONE);
+            holder.genderData.setVisibility(View.GONE);
+
+            holder.startYear.setText(String.format(String.valueOf(ventenFilters.get(position).getStartYear())));
+            holder.endYear.setText(String.format(String.valueOf(ventenFilters.get(position).getEndYear())));
+
+
+            ArrayList<String> arrayListCountry = new ArrayList<>();
+            arrayListCountry.add(ventenFilters.get(position).getCountries().toString());
+            StringBuilder builderCountry = new StringBuilder();
+
+            for (String valueCountry : arrayListCountry) {
+                builderCountry.append(valueCountry);
+
+            }
+            String formattedStringCountry = builderCountry.toString()
+                    .replace("[", "")
+                    .replace("]", "")
+                    .replace(",", "")
+                    .trim();
+            holder.countries.setText(formattedStringCountry);
+
+
+            ArrayList<String> arrayList = new ArrayList<>();
+            arrayList.add(ventenFilters.get(position).getColors().toString());
+            StringBuilder builder = new StringBuilder();
+
+            for (String value : arrayList) {
+                builder.append(value);
+
+
+            }
+            String formattedString = builder.toString()
+                    .replace("[", "")
+                    .replace("]", "")
+                    .replace(",", "")
+                    .trim();
+            holder.colors.setText(formattedString);
+
+        }else if (ventenFilters.get(position).getCountries().isEmpty()){
+            holder.countries.setVisibility(View.GONE);
+            holder.countriesData.setVisibility(View.GONE);
+
+            holder.gender.setText(ventenFilters.get(position).getGender());
+            holder.startYear.setText(String.format(String.valueOf(ventenFilters.get(position).getStartYear())));
+            holder.endYear.setText(String.format(String.valueOf(ventenFilters.get(position).getEndYear())));
+
+
+            ArrayList<String> arrayList = new ArrayList<>();
+            arrayList.add(ventenFilters.get(position).getColors().toString());
+            StringBuilder builder = new StringBuilder();
+
+            for (String value : arrayList) {
+                builder.append(value);
+
+
+            }
+            String formattedString = builder.toString()
+                    .replace("[", "")
+                    .replace("]", "")
+                    .replace(",", "")
+                    .trim();
+            holder.colors.setText(formattedString);
+        }else if (ventenFilters.get(position).getColors().isEmpty()){
+            holder.colors.setVisibility(View.GONE);
+            holder.colorsData.setVisibility(View.GONE);
+
+            holder.gender.setText(ventenFilters.get(position).getGender());
+            holder.startYear.setText(String.format(String.valueOf(ventenFilters.get(position).getStartYear())));
+            holder.endYear.setText(String.format(String.valueOf(ventenFilters.get(position).getEndYear())));
+
+
+            ArrayList<String> arrayListCountry = new ArrayList<>();
+            arrayListCountry.add(ventenFilters.get(position).getCountries().toString());
+            StringBuilder builderCountry = new StringBuilder();
+
+            for (String valueCountry : arrayListCountry) {
+                builderCountry.append(valueCountry);
+
+            }
+            String formattedStringCountry = builderCountry.toString()
+                    .replace("[", "")
+                    .replace("]", "")
+                    .replace(",", "")
+                    .trim();
+            holder.countries.setText(formattedStringCountry);
 
         }
-        String formattedStringCountry = builderCountry.toString()
-                .replace("[", "")
-                .replace("]", "")
-                .replace(",", "")
-                .trim();
-        holder.countries.setText(formattedStringCountry);
+
+        else {
+            holder.gender.setText(ventenFilters.get(position).getGender());
+            holder.startYear.setText(String.format(String.valueOf(ventenFilters.get(position).getStartYear())));
+            holder.endYear.setText(String.format(String.valueOf(ventenFilters.get(position).getEndYear())));
 
 
-        ArrayList<String>  arrayList = new ArrayList<>();
-        arrayList.add(ventenFilters.get(position).getColors().toString());
-        StringBuilder builder = new StringBuilder();
+            ArrayList<String> arrayListCountry = new ArrayList<>();
+            arrayListCountry.add(ventenFilters.get(position).getCountries().toString());
+            StringBuilder builderCountry = new StringBuilder();
 
-        for (String value : arrayList){
-            builder.append(value);
+            for (String valueCountry : arrayListCountry) {
+                builderCountry.append(valueCountry);
+
+            }
+            String formattedStringCountry = builderCountry.toString()
+                    .replace("[", "")
+                    .replace("]", "")
+                    .replace(",", "")
+                    .trim();
+            holder.countries.setText(formattedStringCountry);
 
 
+            ArrayList<String> arrayList = new ArrayList<>();
+            arrayList.add(ventenFilters.get(position).getColors().toString());
+            StringBuilder builder = new StringBuilder();
+
+            for (String value : arrayList) {
+                builder.append(value);
+
+
+            }
+            String formattedString = builder.toString()
+                    .replace("[", "")
+                    .replace("]", "")
+                    .replace(",", "")
+                    .trim();
+            holder.colors.setText(formattedString);
         }
-        String formattedString = builder.toString()
-                .replace("[", "")
-                .replace("]", "")
-                .replace(",", "")
-                .trim();
-        holder.colors.setText(formattedString);
 
     }
 
@@ -86,6 +218,7 @@ public class FilterAdapter extends RecyclerView.Adapter<FilterAdapter.FilterView
     class FilterViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
 
         TextView startYear, endYear, gender, countries, colors;
+        TextView genderData, countriesData, colorsData;
 
         public FilterViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -95,6 +228,11 @@ public class FilterAdapter extends RecyclerView.Adapter<FilterAdapter.FilterView
             gender = itemView.findViewById(R.id.gender_data);
             countries = itemView.findViewById(R.id.countries_data);
             colors = itemView.findViewById(R.id.colors_data);
+
+            //non data
+            genderData = itemView.findViewById(R.id.car_gender);
+            countriesData = itemView.findViewById(R.id.countries);
+            colorsData = itemView.findViewById(R.id.colors);
 
             itemView.setOnClickListener(this);
         }
