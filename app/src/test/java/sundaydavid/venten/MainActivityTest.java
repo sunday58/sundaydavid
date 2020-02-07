9a9e73ab3fd0021f581305c5a9ac22be2ce8250d
sundaydavid <sundaydavid989@gmail.com>
2020-02-07 18:07:27 +0100

    finished text to get filters

--- app/src/test/java/sundaydavid/venten/MainActivityTest.java
+++ app/src/test/java/sundaydavid/venten/MainActivityTest.java
@@ -1,71 +1,46 @@
 package sundaydavid.venten;
 
-import org.junit.After;
+import org.hamcrest.Matchers;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
-import org.mockito.invocation.InvocationOnMock;
-import org.mockito.stubbing.Answer;
-
-import java.io.IOException;
-import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
 
-import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import sundaydavid.venten.data.Api;
-import sundaydavid.venten.data.ApiInterface;
+
 import sundaydavid.venten.data.VentenFilter;
 
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+
 
 public class MainActivityTest {
 
-    private List<VentenFilter> books;
-    private MainActivity booksPresenter;
-
-    @Mock
-    ApiInterface bookService;
-
-    @Mock
-    Call<List<VentenFilter>> mockCall;
-
-    @Mock
-    ResponseBody responseBody;
-
-    @Captor
-    ArgumentCaptor<Callback<List<VentenFilter>>> argumentCaptor;
+   private List<VentenFilter> ventenFilters;
 
 
-
-    @Before
-    public void setUp(){
-        MockitoAnnotations.initMocks(this);
-
-        booksPresenter = new MainActivity();
-        books = Collections.singletonList(new VentenFilter());
-    }
 
     @Test
     public void textApiResponse(){
 
-        when(bookService.getVentenFilter()).thenReturn(mockCall);
-        Response<List<VentenFilter>> res = Response.success(books);
+        (Api.getClient().getVentenFilter()).enqueue(new Callback<List<VentenFilter>>() {
+            @Override
+            public void onResponse(Call<List<VentenFilter>> call, Response<List<VentenFilter>> response) {
+                ventenFilters = response.body();
 
-        booksPresenter.getUserListData();
+                Assert.assertNotNull("List should not be empty", ventenFilters);
+                Assert.assertEquals("correct size", 5, ventenFilters.size());
 
-        verify(mockCall).enqueue(argumentCaptor.capture());
-        argumentCaptor.getValue().onResponse(null, res);
+            }
 
-        verify(booksPresenter.ventenFilters).get(0);
+            @Override
+            public void onFailure(Call<List<VentenFilter>> call, Throwable t) {
+
+            }
+        });
+
 
     }
+
 }
